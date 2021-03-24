d053aecdafaa2ca89c7910fa916571979db48baa
saadet <saadet@10.129.17.68>
2021-03-24 12:42:46 +0100

    Commit uploading all the files

---
+++ Assignments/src/assignment2Variables/Question7.java
@@ -0,0 +1,21 @@
+package assignment2Variables;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		
+		//Declare and initialize an integer variable i and a floating-point variable f.  
+		//Write a statement that writes both of their values to console in the following format:
+		//i=value-of-i f=value-of-f
+		//Ex: Assume i = 9 and f = 33.4
+        //The output should be: i=9 f=33.4
+		
+		int i; float f;
+		
+		i = 33; f = 11.11f;
+		
+		System.out.println("i=" + i + "  " + "f=" + f );
+
+	}
+
+}
