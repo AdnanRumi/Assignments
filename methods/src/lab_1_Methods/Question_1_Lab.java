883049670aa3a0d1e283668934ae825fa6d345b3
saadet <saadet@192.168.1.130>
2021-04-08 18:48:03 +0200

    Commit this one

--- methods/src/lab_1_Methods/Question_1_Lab.java
+++ methods/src/lab_1_Methods/Question_1_Lab.java
@@ -11,18 +11,46 @@ public class Question_1_Lab {
      strings("Peace", 7);
 
 
-     
+          
      
 
 	}
 
 	public static void strings(String string, int i) {
 		
-		System.out.println("Peace");
+		if(i == 1) {
 			
+			System.out.println("Peace");
 			
+		}else if(i == 2) {
+			
+			System.out.println("Peace" + "Peace");
+			
+		}else if(i == 3) {
+			
+			System.out.println("Peace" + "Peace" + "Peace");
+			
+		}else if (i == 4) {
+			
+			System.out.println("Peace" + "Peace" + "Peace" + "Peace");
+			
+		}else if (i == 5) {
+			
+			System.out.println("Peace" + "Peace" + "Peace" + "Peace" + "Peace");
+
+		}else if (i == 6) {
+			
+			System.out.println("Peace" + "Peace" + "Peace" + "Peace"+ "Peace" + "Peace");
+
+		}else if(i == 7) {
+			
+			System.out.println("Peace" + "Peace" + "Peace" + "Peace"+ "Peace" + "Peace" + "Peace");
+
 		}
-		
-		
+			
+			
 	}
+		
+		
+}
 
