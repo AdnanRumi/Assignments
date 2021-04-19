2498533098f236eebe55e9f8749a497f81bd11ee
saadet <saadet@10.129.17.68>
2021-04-19 14:50:15 +0200

    Commit

--- Assignments/src/assignment_3_ArithmeticOperators/Question_6.java
+++ Assignments/src/assignment_3_ArithmeticOperators/Question_6.java
@@ -11,17 +11,19 @@ public class Question_6 {
            Number of milligrams in drink: 500
            It would take about 20 drinks for a lethal overdose */
        
-		int oneDrinkCaffeine, lethalOverdose, oneGram, milligram;
+		int oneDrink, lethalOverdose, oneGram, milligram, numberOfDrinksOverdose;
 		
-		oneDrinkCaffeine = 500;       
+		oneDrink = 500;       
 		
 		oneGram = 1000 ;
 		
-		lethalOverdose = oneGram * 10;  
+		lethalOverdose = oneGram * 10; 
 		
-		System.out.println("In one drink there are " + oneDrinkCaffeine + " milligrams of caffeine");
+		numberOfDrinksOverdose = lethalOverdose / oneDrink;
 		
-		System.out.println(lethalOverdose + " milligrams of caffeine is the leathal overdose and is found in average 20 drinks");
+		System.out.println("In one drink there are " + oneDrink + " milligrams of caffeine");
+		
+		System.out.println(lethalOverdose + " milligrams of caffeine is the leathal overdose and is found in average " + numberOfDrinksOverdose + " drinks");
 		
 		
 		
